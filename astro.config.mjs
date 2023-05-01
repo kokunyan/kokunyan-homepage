24edb7887ebbfbb0c47f13dfa36125dd7b7f1805
Bogdan Kokunin <bkokunin@gmail.com>
2023-05-01 16:15:29 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), image(), mdx(), robotsTxt(), sitemap(), prefetch()]
+});
