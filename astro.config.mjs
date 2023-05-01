33b4f422592f8f6261eed63fe08bbaafb93d59e3
Bogdan Kokunin <bkokunin@gmail.com>
2023-05-01 16:31:27 +0300

    Add more vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from "@astrojs/vercel/serverless";
 import prefetch from "@astrojs/prefetch";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image(), mdx(), robotsTxt(), sitemap(), prefetch()]
+  integrations: [react(), tailwind(), image(), mdx(), robotsTxt(), sitemap(), prefetch()],
+  adapter: vercel(),
+  output: "server"
 });
